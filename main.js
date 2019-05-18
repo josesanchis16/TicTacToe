a73164350a11a48682962787523aa1dc74a77f59
Jose <sanchisbeldajose@gmail.com>
2019-05-18 04:13:16 +0200

    creado el click del jugador

--- main.js
+++ main.js
@@ -1,24 +1,75 @@
-const btnPos1 = {
-    document.getElementById('pos1'),
-    
-}
-const btnPos2 = document.getElementById('pos2');
-const btnPos3 = document.getElementById('pos3');
-const btnPos4 = document.getElementById('pos4');
-const btnPos5 = document.getElementById('pos5');
-const btnPos6 = document.getElementById('pos6');
-const btnPos7 = document.getElementById('pos7');
-const btnPos8 = document.getElementById('pos8');
-const btnPos9 = document.getElementById('pos9');
+class Boton {
+    constructor(btn) {
+        this.btn = btn;
+        this.pressed = false;
+    }
 
-btnPos1.onclick = function () {
+    getBtn = function () {
+        return this.btn;
+    }
+
+    setPressed = function (pressed) {
+        this.pressed = pressed;
+    }
+
+    isPressed = function () {
+        return this.pressed;
+    }
+}
+
+const btnPos1 = new Boton(document.getElementById('pos1'));
+const btnPos2 = new Boton(document.getElementById('pos2'));
+const btnPos3 = new Boton(document.getElementById('pos3'));
+const btnPos4 = new Boton(document.getElementById('pos4'));
+const btnPos5 = new Boton(document.getElementById('pos5'));
+const btnPos6 = new Boton(document.getElementById('pos6'));
+const btnPos7 = new Boton(document.getElementById('pos7'));
+const btnPos8 = new Boton(document.getElementById('pos8'));
+const btnPos9 = new Boton(document.getElementById('pos9'));
+
+btnPos1.getBtn().onclick = function () {
     dibujarCruz(btnPos1);
 }
 
+btnPos2.getBtn().onclick = function () {
+    dibujarCruz(btnPos2);
+}
+
+btnPos3.getBtn().onclick = function () {
+    dibujarCruz(btnPos3);
+}
+
+btnPos4.getBtn().onclick = function () {
+    dibujarCruz(btnPos4);
+}
+
+btnPos5.getBtn().onclick = function () {
+    dibujarCruz(btnPos5);
+}
+
+btnPos6.getBtn().onclick = function () {
+    dibujarCruz(btnPos6);
+}
+
+btnPos7.getBtn().onclick = function () {
+    dibujarCruz(btnPos7);
+}
+
+btnPos8.getBtn().onclick = function () {
+    dibujarCruz(btnPos8);
+}
+
+btnPos9.getBtn().onclick = function () {
+    dibujarCruz(btnPos9);
+}
+
 function dibujarCruz(pos) {
-    let element = document.createElement('i');
-    element.setAttribute('class', 'fas fa-times fa-4x');
-    console.log(element);
-    pos.appendChild(element);
-    console.log(pos);
+    if (!pos.isPressed()) {
+        let element = document.createElement('i');
+        element.setAttribute('class', 'fas fa-times fa-4x');
+        pos.getBtn().appendChild(element);
+        pos.setPressed(true);
+    } else {
+        console.log('El boton seleccionado ya esta presionado');
+    }
 }
